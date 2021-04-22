611c10e56cbad4c9b0bc38b224150493f860f543
mihir <mihirpaghdal@gmail.com>
2021-04-23 00:52:59 +0530

    Ui changed

--- QuadB/app/src/main/java/com/example/quadb/MainActivity.java
+++ QuadB/app/src/main/java/com/example/quadb/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner mCuisine;
+
+    ImageView mPizza;
+    ImageView mBurger;
+    ImageView mFries;
+    ImageView mHealthy;
+    ImageView mSandwich;
+    ImageView mCake;
+    ImageView mChaat;
+    ImageView mChinese;
+    ImageView mPaypal;
+    ImageView mPaytm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 getResources().getStringArray(R.array.cuisines));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mCuisine.setAdapter(adapter);
+        mBurger = (ImageView) findViewById(R.id.iv_burger);
+        mPaypal = (ImageView) findViewById(R.id.offer_paypal);
+        mCake = (ImageView) findViewById(R.id.iv_cake);
+        mChaat = (ImageView) findViewById(R.id.iv_chaat);
+        mChinese = (ImageView) findViewById(R.id.iv_chinese);
+        mPaytm = (ImageView) findViewById(R.id.offer_paytm);
+        mPizza = (ImageView) findViewById(R.id.iv_pizza);
+        mHealthy = (ImageView) findViewById(R.id.iv_healthy);
+        mFries = (ImageView) findViewById(R.id.iv_fries);
+        mSandwich = (ImageView) findViewById(R.id.iv_sandwich);
     }
 }
