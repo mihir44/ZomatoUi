a0763ba6520e35f06cc327a17da0b66a0ea3b5a9
mihir <mihirpaghdal@gmail.com>
2021-04-23 12:51:43 +0530

    image shaped in circle

--- QuadB/app/src/main/java/com/example/quadb/MainActivity.java
+++ QuadB/app/src/main/java/com/example/quadb/MainActivity.java
@@ -32,14 +32,34 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mCuisine.setAdapter(adapter);
         mBurger = (ImageView) findViewById(R.id.iv_burger);
+        int burgerId = getResources().getIdentifier("@drawable/burger",null, this.getPackageName());
+        mBurger.setImageResource(burgerId);
         mPaypal = (ImageView) findViewById(R.id.offer_paypal);
+        int paypalId = getResources().getIdentifier("@drawable/offer_paypal",null, this.getPackageName());
+        mPaypal.setImageResource(paypalId);
         mCake = (ImageView) findViewById(R.id.iv_cake);
+        int cakeId = getResources().getIdentifier("@drawable/cake",null, this.getPackageName());
+        mCake.setImageResource(cakeId);
         mChaat = (ImageView) findViewById(R.id.iv_chaat);
+        int chaatId = getResources().getIdentifier("@drawable/chaat",null, this.getPackageName());
+        mChaat.setImageResource(chaatId);
         mChinese = (ImageView) findViewById(R.id.iv_chinese);
+        int chineseId = getResources().getIdentifier("@drawable/chinese",null, this.getPackageName());
+        mChinese.setImageResource(chineseId);
         mPaytm = (ImageView) findViewById(R.id.offer_paytm);
+        int paytmId = getResources().getIdentifier("@drawable/offer_paytm",null, this.getPackageName());
+        mPaytm.setImageResource(paytmId);
         mPizza = (ImageView) findViewById(R.id.iv_pizza);
+        int pizzaId = getResources().getIdentifier("@drawable/pizza",null, this.getPackageName());
+        mPizza.setImageResource(pizzaId);
         mHealthy = (ImageView) findViewById(R.id.iv_healthy);
+        int healthyId = getResources().getIdentifier("@drawable/healthy",null, this.getPackageName());
+        mHealthy.setImageResource(healthyId);
         mFries = (ImageView) findViewById(R.id.iv_fries);
+        int friesId = getResources().getIdentifier("@drawable/fries",null, this.getPackageName());
+        mFries.setImageResource(friesId);
         mSandwich = (ImageView) findViewById(R.id.iv_sandwich);
+        int sandwichId = getResources().getIdentifier("@drawable/sandwich",null, this.getPackageName());
+        mSandwich.setImageResource(sandwichId);
     }
 }
